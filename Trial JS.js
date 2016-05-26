9167c79fe24fe861e1e799e6d94929e7d0cb002a
jmp186 <jmp186@utulsa.edu>
2016-05-26 16:25:23 -0600

    2nd Save 5/26

---
+++ Trial JS.js
@@ -0,0 +1,9 @@
+function myGlossary(text) {
+  if (document.form1.threeChar.value.length == 3) {
+    return true;
+  } else {
+    alert("Enter exactly three characters. " +
+    document.form1.threeChar.value + " is not valid.");
+    return false;
+  }
+}
